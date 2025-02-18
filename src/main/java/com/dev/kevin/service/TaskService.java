9f4dc0576b65241a2b4d7f5669b9a78b8ac82a47
Kevin <kevin.silva2010.ks@gmail.com>
2025-02-18 09:03:06 -0300

    Criando Serviços das Tasks

---
+++ src/main/java/com/dev/kevin/service/TaskService.java
@@ -0,0 +1,54 @@
+package com.dev.kevin.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dev.kevin.model.Task;
+import com.dev.kevin.model.User;
+import com.dev.kevin.repository.TaskRepository;
+
+@Service
+public class TaskService {
+
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private TaskRepository taskRepository;
+	
+	
+	public Task findById(Long id) {
+		Optional<Task> task = this.taskRepository.findById(id);
+		return task.orElseThrow(() -> new RuntimeException
+				("Tarefa Não encontrada! id:" + id + ", Tipo " + Task.class.getName()));
+	}
+	
+	@Transactional
+	public Task create(Task obj) {
+		User user = this.userService.findById(obj.getUser().getId());
+		obj.setId(null);
+		obj.setUser(user);
+		obj = this.taskRepository.save(obj);
+		return obj;	
+	}
+	
+	@Transactional
+	public Task update(Task obj) {
+		Task newObj = findById(obj.getId());
+		newObj.setDescricao(obj.getDescricao());
+		return this.taskRepository.save(newObj);
+	}
+	
+	public void delete(Long id) {
+		findById(id);
+		try {
+			this.taskRepository.deleteById(id);
+		} catch (Exception e) {
+			throw new RuntimeException
+				("Não é possivel deletar pois há entidades relacionadas!");
+		}
+		
+	}
+}
